461305be67d751a6cf6c988d3a7da990fc18a78f
khanh <nguyenvietnamkhanh@gmail.com>
2011-10-13 02:16:12 +0700

    fixed issue: filter search - part 13

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -25,13 +25,13 @@ class LocationsController < ApplicationController
     coordinates = @latlng.blank? ? session[:search] : @latlng
     
     @near_your_locations = HTTParty.get("https://maps.googleapis.com/maps/api/place/search/json?location=#{coordinates.join(',')}&types=#{types}&radius=#{RADIUS}&sensor=false&key=AIzaSyA1mwwvv3NAL_N7gNRf_0uqK2pfiXEqkZc")    
-    @locations = Location.near(coordinates, 100)    
+    @locations = Location.near(coordinates, 2000)    
   end
   
   # TODO
   def search
     @latlng = [params[:latitude].to_f, params[:longitude].to_f]
-    @locations = Location.near(@latlng, 100)
+    @locations = Location.near(@latlng, 2000)
     render :partial => "locations"
   end
   

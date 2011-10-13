2618c3cc0c315c1caee95e73e4ccad4d9dac1310
khanh <nguyenvietnamkhanh@gmail.com>
2011-10-13 23:13:43 +0700

    upload image to server

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+require 'csv'
+
+# import country data from countries cvs file
+puts "importing adv data..."
+Advertise.delete_all
+CSV.foreach("#{Rails.root}/db/advertises.csv") do |row|
+  adv = Advertise.new(:id => row[0], :business_type => row[1], 
+    :photo_file_name => row[2], :photo_content_type => row[3], 
+    :photo_file_size => row[4], :photo_updated_at => row[5],
+    :created_at => row[6], :updated_at => row[7])
+  adv.save
+end

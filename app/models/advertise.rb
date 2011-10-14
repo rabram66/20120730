085c0147b33604e9093bf76bf63ddc0d0a2f4fee
khanh <nguyenvietnamkhanh@gmail.com>
2011-10-14 12:01:08 +0700

    implement upload to S3

--- app/models/advertise.rb
+++ app/models/advertise.rb
@@ -1,3 +1,9 @@
 class Advertise < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "50x50>" }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "50x50>" },
+    :storage => :s3,
+    :bucket => 'nearbythis',
+    :s3_credentials => {
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    }
 end

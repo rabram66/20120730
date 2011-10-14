33b5ab953485aaaf0a11421765de4ddd6f912dc0
khanh <nguyenvietnamkhanh@gmail.com>
2011-10-14 17:59:27 +0700

    fixed bug: search near location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -24,8 +24,11 @@ class LocationsController < ApplicationController
     @latlng = Geocoder.coordinates(DEFAULT_LOCATION) if @latlng.blank? &&  session[:search].blank?    
     coordinates = @latlng.blank? ? session[:search] : @latlng
     
-    @near_your_locations = HTTParty.get("https://maps.googleapis.com/maps/api/place/search/json?location=#{coordinates.join(',')}&types=#{types}&radius=#{RADIUS}&sensor=false&key=AIzaSyA1mwwvv3NAL_N7gNRf_0uqK2pfiXEqkZc")    
-    @locations = Location.near(coordinates, 300)    
+    @near_your_locations = HTTParty.get("https://maps.googleapis.com/maps/api/place/search/json?location=#{coordinates.join(',')}&types=#{types}&radius=#{RADIUS}&sensor=false&key=AIzaSyA1mwwvv3NAL_N7gNRf_0uqK2pfiXEqkZc")
+		begin 
+	    @locations = Location.near(coordinates, 300)    
+		rescue
+		end
   end
   
   # TODO

22e80b7eb555d5d84de2665a8d4729718ee1d485
Khanh Nguyen <nguyenvietnamkhanh@gmail.com>
2011-10-18 22:17:48 +0700

    add events authetication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111018143538) do
+ActiveRecord::Schema.define(:version => 20111018151131) do
 
   create_table "advertises", :force => true do |t|
     t.string   "business_type"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20111018143538) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "locations", :force => true do |t|

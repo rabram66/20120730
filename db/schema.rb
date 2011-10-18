a114734efd8b33944fde61c15b09a82636e60dfa
Khanh Nguyen <nguyenvietnamkhanh@gmail.com>
2011-10-18 15:46:37 +0700

    add roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111018030837) do
+ActiveRecord::Schema.define(:version => 20111018042135) do
 
   create_table "advertises", :force => true do |t|
     t.string   "business_type"
@@ -56,6 +56,17 @@ ActiveRecord::Schema.define(:version => 20111018030837) do
     t.string   "yourstuff"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
   end
 
   create_table "users", :force => true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,25 +11,30 @@ require 'csv'
 #  adv.save
 #end
 
-puts "update general location....."
-eat_drink = ["bakery", "coke", "bar", "cafe", "food", "meal takeaway", "restaurant"]  
-  
-relax_care = ["amusement park", "aquarium", "art gallery", "beauty salon", "bowling alley",
-  "casino", "gym", "hair care", "health", "movie theater", "museum", "night club", "park", "spa", "zoo"]  
-  
-shop_find = ["atm", "bank", "bicycle store", "book store", "bus station", "clothing store", "convenience store" ,
-  "department store", "electronics store", "establishment", "florist", "gas station", "grocery", "supermarket",
-  "hardware store", "home goods store", "jewelry store", "library", "liquor store", "locksmith", "pet store",
-  "pharmacy", "shoe store", "shopping mall", "store"]
-locations = Location.all
+#puts "update general location....."
+#eat_drink = ["bakery", "coke", "bar", "cafe", "food", "meal takeaway", "restaurant"]  
+#  
+#relax_care = ["amusement park", "aquarium", "art gallery", "beauty salon", "bowling alley",
+#  "casino", "gym", "hair care", "health", "movie theater", "museum", "night club", "park", "spa", "zoo"]  
+#  
+#shop_find = ["atm", "bank", "bicycle store", "book store", "bus station", "clothing store", "convenience store" ,
+#  "department store", "electronics store", "establishment", "florist", "gas station", "grocery", "supermarket",
+#  "hardware store", "home goods store", "jewelry store", "library", "liquor store", "locksmith", "pet store",
+#  "pharmacy", "shoe store", "shopping mall", "store"]
+#locations = Location.all
+#
+#locations.each do |location|
+#   if eat_drink.include?(location.types) 
+#     location.general_type = "Eat/Drink"
+#   elsif relax_care.include?(location.types)
+#     location.general_type = "Relax/Care"
+#   elsif shop_find.include?(location.types)
+#     location.general_type = "Shop/Find"
+#   end    
+#  location.save
+#end
 
-locations.each do |location|
-   if eat_drink.include?(location.types) 
-     location.general_type = "Eat/Drink"
-   elsif relax_care.include?(location.types)
-     location.general_type = "Relax/Care"
-   elsif shop_find.include?(location.types)
-     location.general_type = "Shop/Find"
-   end    
-  location.save
+puts "create roles..."
+["User", "Promoter", "Admin"].each do |role_name|
+  Role.create(:name => role_name)
 end

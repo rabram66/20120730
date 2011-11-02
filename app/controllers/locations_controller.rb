ec15abd8a803d77509d834390e2b71df88404197
Khanh Nguyen <nguyenvietnamkhanh@gmail.com>
2011-11-02 09:26:00 +0700

    update details page - 13

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -264,7 +264,7 @@ class LocationsController < ApplicationController
   def get_logo(details, location)   
     adv = nil
     unless location.blank?      
-      adv = Advertise.find_by_sql("SELECT * FROM advertises where address_name like \"%#{location.city}, #{location.state}%\" and business_name like \"%#{location.name}%\" ")[0]
+      adv = Advertise.find_by_sql("SELECT * FROM advertises where address_name like \"%#{location.city}, #{location.state}%\" and business_name like \"%#{location.name}%\"")[0]
       adv = Advertise.where("business_name like \"%#{location.name}%\"").first() if adv.blank?
       adv = Advertise.where("address_name like \"%#{location.city}, #{location.state}%\"").first() if adv.blank?
       adv = Advertise.where("business_type = \"#{location.types}\"").first() if adv.blank?
@@ -272,7 +272,7 @@ class LocationsController < ApplicationController
       loc = details['result']       
       if loc['vicinity'] != nil && loc['name'] != nil
         add, city = loc['vicinity'].split(",")          
-        adv = Advertise.find_by_sql("SELECT * FROM advertises where (address_name like \"%#{city.strip}%\" or address_name like \"%#{add.strip}%\") and business_name like \"%#{loc["name"]}%\" ")[0]
+        adv = Advertise.find_by_sql("SELECT * FROM advertises where (address_name like \"%#{city.strip}%\" or address_name like \"%#{add.strip}%\") and business_name like \"%#{loc["name"]}%\"")[0]
         adv = Advertise.where("business_name like \"%#{loc["name"]}%\"").first() if adv.blank?          
       end      
     end

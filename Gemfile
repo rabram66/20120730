77d74802b1aaaf51b539165938a4135823bef365
Bill Siggelkow <bsiggelkow@mac.com>
2011-11-21 18:12:49 -0500

    Flesh out mobile detail (locations, deals, and events) pages

--- Gemfile
+++ Gemfile
@@ -17,5 +17,6 @@ gem "aws-s3", "~> 0.6.2"
 gem "devise"
 gem "mobile-fu", "~> 0.2.1"
 gem "faster_haversine", "~> 0.1.3" # Calculating distance between geocodes
+gem 'stamp'
 #gem 'ruby-debug19'
 
--- app/models/deal.rb
+++ app/models/deal.rb
@@ -3,7 +3,7 @@ class Deal
   API_KEY = "zZnf9zms8Kxp6BPE"
   YIPIT_URL = "http://api.yipit.com/v1/deals/?key=#{API_KEY}&lat=%s&lon=%s"
 
-  attr_reader :title, :yipit_url
+  attr_reader :title, :url, :mobile_url, :thumbnail_url, :latitude, :longitude
 
   def initialize(attrs={})
     attrs.each do |k,v|
@@ -21,7 +21,13 @@ class Deal
         res = ActiveSupport::JSON.decode(response.parsed_response)
         if res['response'] && res['response']['deals']
           deals = res['response']['deals'].map do |deal|
-            Deal.new(:title => deal['title'], :yipit_url => deal['url'])
+            
+            Deal.new(
+              :title      => deal['title'], 
+              :url        => deal['url'],
+              :mobile_url => deal['mobile_url'],
+              :thumbnail_url => deal['images']['image_small']
+            )
           end
         end
       else

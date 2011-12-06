33d5a3b118cc1a12dd6f0300e754724dc5f0f6df
Bill Siggelkow <bsiggelkow@mac.com>
2011-12-05 20:15:06 -0500

    Try variant for loading database

--- lib/tasks/locations_spreadsheet.rb
+++ lib/tasks/locations_spreadsheet.rb
@@ -1,31 +1,35 @@
 class LocationsSpreadsheet
   
-  attr_reader :file, :book
+  attr_reader :filename, :book
 
   def initialize(args)
-    @file = args[:file]
+    @filename = args[:file]
   end
   
   def import
-    puts "Importing locations from #{file}"
-    @book = Spreadsheet.open file
-    sheet = book.worksheet 0 # First worksheet
-    sheet.each(1) do |row| # skip first row
-      # COMPANY_NAME, ADDRESS, CITY, STATE, ZIP, Category, Twitter_Name
-      begin
-        Location.new(
-          :name         => row[0].strip,
-          :address      => row[1].strip,
-          :city         => row[2].strip,
-          :state        => row[3].strip,
-          :types        => category_to_type(row[5]),
-          :twitter_name => row[6].strip
-        ).save!
-        print '.'; $stdout.flush
-      rescue
-        puts "Error processing #{row.inspect}: #{$!}"
+    puts "Importing locations from #{filename}"
+    File.open(filename,'r') do |file|
+      @book = Spreadsheet.open file
+      sheet = book.worksheet 0 # First worksheet
+      sheet.each(1) do |row| # skip first row
+        # COMPANY_NAME, ADDRESS, CITY, STATE, ZIP, Category, Twitter_Name
+        begin
+          next unless row[0] # Skip empty rows
+          Location.new(
+            :name         => row[0].strip,
+            :address      => row[1].strip,
+            :city         => row[2].strip,
+            :state        => row[3].strip,
+            :types        => category_to_type(row[5]),
+            :twitter_name => row[6].strip
+          ).save!
+          print '.'; $stdout.flush
+        rescue
+          puts "Error processing #{row.inspect}: #{$!}"
+        end
       end
     end
+    puts "\nDone"
   end
 
   private

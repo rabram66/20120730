7e696649e6beff21ddacdbeb5bb63445bb6328e0
Bill Siggelkow <bsiggelkow@mac.com>
2011-12-21 22:28:04 -0500

    Role filter replaced with CanCan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,6 @@ class ApplicationController < ActionController::Base
   
   has_mobile_fu
 
-  def role
-    if ['places', 'devise/sessions', 'devise/registrations', 'devise/passwords'].include? params[:controller]
-      # do nothing; no restrictions
-    else
-      unless current_user.blank?
-        permission = "access denied"
-        if current_user.role? :admin
-          permission = "OK"
-        elsif current_user.role? :promoter
-          permission = params["controller"] == 'events' ? "OK" : "access denied"
-        end
-        redirect_to root_url, :notice => permission if permission.eql?("access denied")
-      else
-        redirect_to root_url
-      end
-    end
-  end
-
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :notice => 'Access Denied'
   end

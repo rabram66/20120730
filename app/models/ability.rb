3277fc94420e49b4a3aa41691e48ae2b9664723e
Bill Siggelkow <bsiggelkow@mac.com>
2011-12-31 15:04:00 -0500

    Allow guest users to show events

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     cannot :manage, :all
-    # can :read, :all
+    can :show, Event
     if user.role? :admin
       can :manage, :all
     elsif user.role? :promoter

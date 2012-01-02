d5c9fa3c97a3ff40400575029e6583eb8e9730b9
Bill Siggelkow <bsiggelkow@mac.com>
2012-01-02 10:04:33 -0500

    Fix problem where map not displayed unless cookied

--- public/javascripts/directions.js
+++ public/javascripts/directions.js
@@ -2,25 +2,33 @@
   var directionsService = new google.maps.DirectionsService();
   var map;
   var marker;
-  var bussiness_address;
+
   function initialize() {
     directionsDisplay = new google.maps.DirectionsRenderer();
-    var orig_latlng = new google.maps.LatLng($('#orig_lat').val(), $('#orig_lng').val());
-    var latlng = new google.maps.LatLng($('#dest_lat').val(), $('#dest_lng').val());
+
+    var dest_latlng = new google.maps.LatLng($('#dest_lat').val(), $('#dest_lng').val());
+    if($('#orig_lat').length) {
+      var orig_latlng = new google.maps.LatLng($('#orig_lat').val(), $('#orig_lng').val());
+    } else {
+      var orig_latlng = dest_latlng;
+    }
+
     var myOptions = {
       zoom:10,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
-      center: bussiness_address
+      center: dest_latlng
     };
+
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
     directionsDisplay.setMap(map);
     directionsDisplay.setPanel(document.getElementById('directions-panel-detail'));
               
     var request = {
       origin: orig_latlng,
-      destination: latlng,
+      destination: dest_latlng,
       travelMode: google.maps.DirectionsTravelMode.WALKING
     };
+
     directionsService.route(request, function(response, status) {
       if (status == google.maps.DirectionsStatus.OK) {
         directionsDisplay.setDirections(response);

cc72d6c3a7b7abf42c5243a69c50c83750c01ccd
Bill Siggelkow <bsiggelkow@mac.com>
2012-01-02 18:16:48 -0500

    clean up event date time handling

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,21 +8,29 @@ class Event < ActiveRecord::Base
   validates_presence_of :name, :address, :city, :state, :description
   attr_accessible :name, :address, :city, :state, :description,
                   :latitude, :longitude, :user_id, :full_address, :start_date,
-                  :tags, :end_date
+                  :tags, :end_date, :venue, :category
   validates :category, :inclusion => {:in => CATEGORIES, :allow_blank => true}
 
   belongs_to :user
   geocoded_by :full_address
 
   scope :upcoming, where("(start_date ISNULL AND end_date ISNULL) OR (start_date >= :today OR end_date >= :today)", {:today => Date.today})
+
+  before_save :parse_dates
+  
+  # before_save do
+  #   puts "before_save: End date: #{self.end_date}"
+  # end
+  # 
+  # after_save do
+  #   puts "after_save: End date: #{self.end_date}"
+  # end
   
   after_validation do
     geocode if !(ADDRESS_ATTRS & changes.keys).empty? || latitude.blank? || longitude.blank?
     normalize_tags unless tags.blank?
   end
 
-  before_save :parse_dates
-  
   def full_address=(value)
     parse_full_address(value)
   end
@@ -47,11 +55,11 @@ class Event < ActiveRecord::Base
   private
   
   def parse_dates
-    self.start_date = Chronic::parse(start_date_before_type_cast) 
+    self.start_date = Chronic::parse(start_date_before_type_cast) unless start_date_before_type_cast.blank?
     unless end_date_before_type_cast.blank?
-      # If only time was passed; set date to date of start_date
-      date_to_parse = end_date_before_type_cast =~ /^\d?\d\:\d\d\s?(am|pm)$/i ? "#{self.start_date.to_date.to_s} #{end_date_before_type_cast}" : end_date_before_type_cast
-      self.end_date = Chronic::parse(date_to_parse)
+      parsed_date = Chronic::parse(end_date_before_type_cast)
+      date = Time.new(start_date.year, start_date.month, start_date.day, end_date.hour, end_date.min, end_date.sec, end_date.utc_offset)
+      self.end_date = date < start_date ? date + 1.day : date
     end
   end
 

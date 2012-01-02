d5c1c7b778ec81e1b8867abed76325cf20e37ca6
Bill Siggelkow <bsiggelkow@mac.com>
2012-01-02 10:23:33 -0500

    Set default timezone to Eastern

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,8 @@ module LocationDB
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # TODO: A longer term solution will be detect the user's timezone from the browser
+    config.time_zone = 'Eastern Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]

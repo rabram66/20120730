4c96c4398e353200a9b99a3b60bb5df7c419347a
Bill Siggelkow <bsiggelkow@mac.com>
2012-01-07 22:38:19 -0500

    Improved unit tests for location and user

--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -5,28 +5,32 @@ class LocationTest < ActiveSupport::TestCase
   setup do
     @location = locations(:one)
   end
+
+  context 'geocoding' do
+    should "not geocode when non-geographic attributes are changed" do
+      @location.email = 'foo@bar.com'
+      @location.expects(:geocode).never
+      Place.expects(:delete).never
+      Place.expects(:add).never
+      assert @location.save!
+    end
   
-  test "should not geocode when non-geographic attributes are changed" do
-    @location.email = 'foo@bar.com'
-    @location.expects(:geocode).never
-    Place.expects(:delete).never
-    Place.expects(:add).never
-    @location.save!
-  end
-  
-  ['city','state','address'].each do |attr|
-    test "should geocode when '#{attr}' changed" do
-      @location.send("#{attr}=", 'test')
-      @location.expects(:geocode).once
-      Place.expects(:delete).once
-      Place.expects(:add).once
-      @location.save!
+    ['city','state','address'].each do |attr|
+      should "geocode when '#{attr}' changed" do
+        @location.send("#{attr}=", 'test')
+        @location.expects(:geocode).once
+        Place.expects(:delete).once
+        Place.expects(:add).once
+        @location.save!
+      end
     end
   end
 
-  test "should delete reference when destroyed" do
-    Place.expects(:delete).once
-    @location.destroy
+  context "google places reference" do
+    should "be deleted when location destroyed" do
+      Place.expects(:delete).once
+      @location.destroy
+    end
   end
   
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context 'roles' do
+    should 'defaults to guest' do
+      user = User.new
+      assert user.role?(:guest)
+    end
+    should 'can be set' do
+      user = User.new
+      user.roles = [:promoter, :admin]
+      assert user.role?(:promoter)
+      assert user.role?(:admin)
+    end
+  end
 end

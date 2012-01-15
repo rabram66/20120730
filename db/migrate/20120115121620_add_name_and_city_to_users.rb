0027e9e23db719405b56ab91ec5c84ae9dab7577
Bill Siggelkow <bsiggelkow@mac.com>
2012-01-15 07:53:36 -0500

    Add name, city, and state to signup

---
+++ db/migrate/20120115121620_add_name_and_city_to_users.rb
@@ -0,0 +1,8 @@
+class AddNameAndCityToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120108031704) do
+ActiveRecord::Schema.define(:version => 20120115121620) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -65,6 +65,10 @@ ActiveRecord::Schema.define(:version => 20120108031704) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "roles_mask",                            :default => 1
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "city"
+    t.string   "state"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

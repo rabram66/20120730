10f6053c6e441d0cf0d54aa3a41f8d0cbb0847e6
Bill Siggelkow <bsiggelkow@mac.com>
2012-01-15 09:41:28 -0500

    Add AWS S3 cred (commented out)

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
   
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => '09DQHTFR13R7DZE9HPG2',
-    :aws_secret_access_key  => '2R3uzh5EiX0PfNGDDZ7mBq/Qt1SSukV9NNDAbULG'
+    :aws_access_key_id      => '09DQHTFR13R7DZE9HPG2', #'AKIAJXYKX3NYSWCCA5BQ',
+    :aws_secret_access_key  => '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' #'MMmQeBAPL8vWedem3yi8EbKofNu1u46gQpUd2Jke' 
   }
   config.fog_directory  = 'bsiggelkow-nearbythis'
 end

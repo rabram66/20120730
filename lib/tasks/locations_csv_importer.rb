4c3c25f6603cbc04c09bc359a1cebd21a0bc4b64
Bill Siggelkow <bsiggelkow@mac.com>
2012-05-10 08:00:31 -0400

    New addresses 5

--- lib/tasks/locations_csv_importer.rb
+++ lib/tasks/locations_csv_importer.rb
@@ -3,7 +3,7 @@ require 'csv'
 class LocationsCsvImporter
   
   def initialize(args)
-    @filename = args[:file] || 'NewAddresses_4.csv'
+    @filename = args[:file] || 'NewAddresses_5.csv'
   end
   
   def import

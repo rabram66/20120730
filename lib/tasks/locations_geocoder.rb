0b3afefd8454fd9db0ece14138047ff3335863d6
Bill Siggelkow <bsiggelkow@mac.com>
2012-06-11 11:57:15 -0400

    Start geocoding New York

--- lib/tasks/locations_geocoder.rb
+++ lib/tasks/locations_geocoder.rb
@@ -1,5 +1,5 @@
 class LocationsGeocoder
   def run
-    Location.where("latitude IS NULL AND city = 'Atlanta'").limit(200).each { |l| l.save }
+    Location.where("latitude IS NULL AND city = 'New York'").limit(200).each { |l| l.save }
   end
 end

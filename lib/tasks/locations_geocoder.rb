442b60e91285348015f0410546759414dda8b37c
Bill Siggelkow <bsiggelkow@mac.com>
2012-06-11 00:09:52 -0400

    Another tweak to geocoder

--- lib/tasks/locations_geocoder.rb
+++ lib/tasks/locations_geocoder.rb
@@ -1,5 +1,5 @@
 class LocationsGeocoder
   def run
-    Location.where("latitude IS NULL AND city = 'Atlanta'").limit(300).each { |l| l.save }
+    Location.where("latitude IS NULL AND city = 'Atlanta'").limit(100).each { |l| l.save }
   end
 end
